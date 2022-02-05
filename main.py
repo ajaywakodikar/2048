3233cef8b0e24b6cd3d2c014c62bbccacadc2f17
Ajay_Wakodikar <ajaywakodikar10@gmail.com>
2022-02-05 19:40:39 +0530

    Game window created

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import pygame as pg
 
+class Game2048:
+    def __init__(self) -> None:
+        self.Height = 540
+        self.Width = 960
+
+        pg.init()
+
+        #window creation
+        self.win = pg.display.set_mode((self.Width, self.Height))
+        pg.display.set_caption("2048")
+
+    def play(self):
+        run = True
+        while run:
+            pg.display.update()
+
+if __name__ == "__main__":
+    game = Game2048()
+    game.play()
